f77d992f79ad9534d2b899a440bc1e512946edaf
ibtissam <ibtissamsaadali17gmail.com>
2024-12-05 10:23:03 +0100

    firebase

--- lib/screens/login.page.dart
+++ lib/screens/login.page.dart
@@ -8,12 +8,9 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-  final _formkey = GlobalKey<FormState>();
-
-  final TextEditingController _mailcontroller = TextEditingController();
-
-  final TextEditingController _passwordcontroller = TextEditingController();
-
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _mailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
   bool _passVisible = false;
 
   String? _validateEmail(String? value) {
@@ -25,7 +22,6 @@ class _LoginPageState extends State<LoginPage> {
     if (!regex.hasMatch(value)) {
       return 'Enter a valid email address!';
     }
-    ;
     return null;
   }
 
@@ -42,88 +38,110 @@ class _LoginPageState extends State<LoginPage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.indigo,
-        title: Text('Login Page',
-            style: TextStyle(fontSize: 30, color: Colors.white)),
+        title: Text(
+          'Login Page',
+          style: TextStyle(fontSize: 24, color: Colors.white),
+        ),
         centerTitle: true,
       ),
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 20.0),
         child: Form(
-          key: _formkey,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Image.asset(
-                'images/logo_flut.png',
-                width: 100,
-                height: 100,
-              ),
-              Text(
-                "Welcome Back",
-                textAlign: TextAlign.center,
-                style: TextStyle(color: Colors.black, fontSize: 20),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              TextFormField(
-                controller: _mailcontroller,
-                decoration: InputDecoration(
-                  labelText: 'Email',
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10)),
-                  prefixIcon: Icon(Icons.email),
+          key: _formKey,
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                SizedBox(height: 40),
+                Center(
+                  child: Image.asset(
+                    'images/logo_flut.png',
+                    width: 100,
+                    height: 100,
+                  ),
                 ),
-                keyboardType: TextInputType.emailAddress,
-                validator: _validateEmail,
-              ),
-              TextFormField(
-                obscureText: _passVisible,
-                controller: _passwordcontroller,
-                decoration: InputDecoration(
+                SizedBox(height: 20),
+                Text(
+                  "Welcome Back",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.black87, fontSize: 22),
+                ),
+                SizedBox(height: 30),
+                TextFormField(
+                  controller: _mailController,
+                  decoration: InputDecoration(
+                    labelText: 'Email',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    prefixIcon: Icon(Icons.email),
+                  ),
+                  keyboardType: TextInputType.emailAddress,
+                  validator: _validateEmail,
+                ),
+                SizedBox(height: 20),
+                TextFormField(
+                  obscureText: !_passVisible,
+                  controller: _passwordController,
+                  decoration: InputDecoration(
                     labelText: 'Password',
                     border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10)),
-                    prefixIcon: Icon(Icons.password),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    prefixIcon: Icon(Icons.lock),
                     suffixIcon: IconButton(
-                        onPressed: () {
-                          setState(() {
-                            _passVisible = !_passVisible;
-                          });
-                        },
-                        icon: _passVisible
-                            ? Icon(Icons.visibility)
-                            : Icon(Icons.visibility_off))),
-                keyboardType: TextInputType.emailAddress,
-                validator: _validatePassword,
-              ),
-              ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          _passVisible = !_passVisible;
+                        });
+                      },
+                      icon: Icon(
+                        _passVisible ? Icons.visibility : Icons.visibility_off,
+                      ),
+                    ),
+                  ),
+                  validator: _validatePassword,
+                ),
+                SizedBox(height: 30),
+                ElevatedButton(
                   style: ElevatedButton.styleFrom(
-                      padding: const EdgeInsets.symmetric(vertical: 20),
-                      backgroundColor: Colors.indigo,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10))),
+                    padding: const EdgeInsets.symmetric(vertical: 15),
+                    backgroundColor: Colors.indigo,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
                   onPressed: () {
-                    if (_formkey.currentState!.validate()) {
+                    if (_formKey.currentState!.validate()) {
                       ScaffoldMessenger.of(context).showSnackBar(
-                          SnackBar(content: Text('Processing login')));
+                        SnackBar(content: Text('Processing login')),
+                      );
                       Navigator.pushNamed(context, '/home');
                     }
                   },
                   child: Text(
                     'Login',
                     style: TextStyle(
-                        color: Colors.white, fontWeight: FontWeight.bold),
-                  )),
-              const SizedBox(height: 10),
-              SizedBox(height: 20),
-              TextButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/register');
-                  },
-                  child: Text('Don\'t have an account? Register here!'))
-            ],
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20),
+                Center(
+                  child: TextButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/register');
+                    },
+                    child: Text(
+                      'Don\'t have an account? Register here!',
+                      style: TextStyle(color: Colors.indigo),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
